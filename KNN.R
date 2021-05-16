2ffc3ecba63f90bef0302c63407a8e82c02bad2d
Johonnatan <48338201+Johonnatan@users.noreply.github.com>
2021-05-16 12:46:43 -0300

    Update KNN.R

--- KNN.R
+++ KNN.R
@@ -1,34 +1,3 @@
-rm(list=ls(all=TRUE)) #Remove objetos da memória do R
-
-#Instala bibliotecas necessarias
-install.packages('mlbench') #biblioteca mlbench disponibiliza varios conjunto de dados, incluindo o Pima Indians Diabetes
-install.packages('caret',dependencies = TRUE) #biblioteca que tem enorme quantidade de ferramentas e algoritmos para trabalhar com machine learning
-install.packages('rpart') #rpart traz o algoritmo de arvore de decisao
-install.packages('rpart.plot') #o rpart.plot serve para visualizar a arvore de decisao gerado pelo rpart
-
-#Carrega as bibliotecas
-library(mlbench)
-library(caret)
-library(rpart)
-library(rpart.plot)
-
-#Carrega o conjunto de dados na memoria do R
-data(PimaIndiansDiabetes)
-
-#Armazena o conjunto de dados PimaIndiansDiabetes em um data frame com o nome dados
-dados <- PimaIndiansDiabetes
-
-#visualiza a media (mean) e outras estatisticas descritivas das variaveis
-summary(dados)
-
-#Separa conjunto de dados para treino e teste para hold-out
-index <- createDataPartition(dados$diabetes, #Variavel resposta
-                             p = 0.8, #Definir percentual para treino
-                             list = F #Manter list = F
-)
-treino <- dados[index,]
-teste <- dados[-index,]
-
 #Remove objetos da memória do R
 rm(list=ls(all=TRUE))
 
